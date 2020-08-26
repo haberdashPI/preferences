87d0e348bb24367b9f78392559a3ac33e460cb0c
David Little <david.frank.little@gmail.com>
2020-08-26 12:59:24 -0400

    julia: don't run alert on edgewater

--- startup.jl
+++ startup.jl
@@ -4,15 +4,22 @@ macro tryusing(mod,expr...)
             using $mod
             $(expr...)
         catch e
-            if hasproperty(e, :msg)
-                @warn(e.msg)
-            else
-                @warn(string("Exception loading ",$(QuoteNode(mod)),": ",e))
+            buffer = IOBuffer()
+            print(buffer, "Exception while loading ")
+            print(buffer, string($(QuoteNode(mod))))
+            println(buffer, ": ")
+            for (exc, bt) in Base.catch_stack()
+                showerror(buffer, exc, bt)
+                println(buffer)
             end
+            @error String(take!(buffer))
         end
     end
 end
 
 @tryusing OhMyREPL enable_autocomplete_brackets(true)
-@tryusing Alert alertREPL(5.0)
 @tryusing Revise
+
+@static if gethostname() != "edgewater"
+    @tryusing Alert alertREPL(5.0)
+end
