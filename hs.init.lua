494832905442d2399f60588b7b3273d9d7cbd572
David Little <david.frank.little@gmail.com>
2021-03-08 12:35:04 -0500

    hammerspoon: add clipboard history

--- hs.init.lua
+++ hs.init.lua
@@ -1,3 +1,14 @@
+Install = hs.loadSpoon("SpoonInstall")
+
+-- Clipboard history
+-- -----------------------------------------------------------------
+
+Install:andUse("TextClipboardHistory", {
+  disable = false, config = { show_in_menubar = false, },
+  hotkeys = { toggle_clipboard = { { "cmd", "shift" }, "v" } },
+  start = true,
+})
+
 -- Vim mode
 -- -----------------------------------------------------------------
 vim = hs.loadSpoon('VimMode')
