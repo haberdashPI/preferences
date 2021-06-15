86a06063436ef2a884ac0382a316bc325146fde5
David Little <david.frank.little@gmail.com>
2021-06-15 12:22:55 -0400

    setup: combine os general commands

--- setup.sh
+++ setup.sh
@@ -19,79 +19,52 @@ if [[ $OSTYPE == "linux-gnu"* ]]; then
     brew install asdf # manages language environments
     brew install fd # modern `find` alternative
     brew install gh # git hub CLI
-
-    # Oh My ZSH and plugins
-    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" --unattended
-
-    # p10k theme
-    { echo 'if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
-    source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"\nfi\n'; cat ~/.zshrc; } > ~/.zshrc.new
-    sed -i 's/ZSH_THEME=".*"/ZSH_THEME="powerlevel10k\/powerlevel10k"/g' ~/.zshrc.new
-    echo "[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh" >> ~/.zshrc.new
-    cp ~/Documents/preferences/p10k.zsh ~/.p10k.zsh
-
-    # ZSH config common across all my machines
-    sed -i '/^source .*oh-my-zsh.sh/i source $HOME\/Documents\/preferences\/zshrc_shared.sh' ~/.zshrc.new
-
-    # plugins
-    git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-
-    # move over the new zshrc file
-    rm ~/.zshrc
-    cp ~/.zshrc.new ~/.zshrc
-
-    # TMUX
-    git clone https://github.com/gpakosz/.tmux.git
-    ln -s -f .tmux/.tmux.conf
-    ln -s ~/Documents/preferences/tmux.conf.local .tmux.conf.local
-
-    # neo vim
-    mkdir -p ~/.conf/nvim
-    ln -s ~/Documents/preferences/vimrc ~/.conf/nvim/init.vim
-    sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-    nvim --headless +silent +PlugInstall +qall
-
-    # setup... 
 elif [[ $OSTYPE == "darwin"* ]]; then
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-    brew install gpg tmux neovim myrepos fzf bat exa asdf fd gh
-    
-    # Oh My ZSH and plugins
-    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" --unattended
-
-    # p10k theme
-    { echo 'if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
-    source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"\nfi\n'; cat ~/.zshrc; } > ~/.zshrc.new
-    sed -i 's/ZSH_THEME=".*"/ZSH_THEME="powerlevel10k\/powerlevel10k"/g' ~/.zshrc.new
-    echo "[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh" >> ~/.zshrc.new
-    cp ~/Documents/preferences/p10k.zsh ~/.p10k.zsh
-
-    # ZSH config common across all my machines
-    sed -i '/^source .*oh-my-zsh.sh/i source $HOME\/Documents\/preferences\/zshrc_shared.sh' ~/.zshrc.new
-
-    # plugins
-    git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-
-    # move over the new zshrc file
-    rm ~/.zshrc
-    cp ~/.zshrc.new ~/.zshrc
-
-    # TMUX
-    git clone https://github.com/gpakosz/.tmux.git
-    ln -s -f .tmux/.tmux.conf
-    ln -s ~/Documents/preferences/tmux.conf.local .tmux.conf.local
-
-    # neo vim
-    mkdir -p ~/.conf/nvim
-    ln -s ~/Documents/preferences/vimrc ~/.conf/nvim/init.vim
-    sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-    nvim --headless +silent +PlugInstall +qall
+    yes | /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    brew install gpg 
+    brew install tmux 
+    brew install neovim 
+    brew install myrepos 
+    brew install fzf 
+    brew install bat 
+    brew install exa 
+    brew install asdf 
+    brew install fd 
+    brew install gh    
 else
     echo "OS Not supported"
 fi
+
+# Oh My ZSH and plugins
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" --unattended
+
+# p10k theme
+{ echo 'if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
+source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"\nfi\n'; cat ~/.zshrc; } > ~/.zshrc.new
+sed -i 's/ZSH_THEME=".*"/ZSH_THEME="powerlevel10k\/powerlevel10k"/g' ~/.zshrc.new
+echo "[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh" >> ~/.zshrc.new
+cp ~/Documents/preferences/p10k.zsh ~/.p10k.zsh
+
+# ZSH config common across all my machines
+sed -i '/^source .*oh-my-zsh.sh/i source $HOME\/Documents\/preferences\/zshrc_shared.sh' ~/.zshrc.new
+
+# plugins
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
+# move over the new zshrc file
+rm ~/.zshrc
+cp ~/.zshrc.new ~/.zshrc
+
+# TMUX
+git clone https://github.com/gpakosz/.tmux.git
+ln -s -f .tmux/.tmux.conf
+ln -s ~/Documents/preferences/tmux.conf.local .tmux.conf.local
+
+# neo vim
+mkdir -p ~/.conf/nvim
+ln -s ~/Documents/preferences/vimrc ~/.conf/nvim/init.vim
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+nvim --headless +silent +PlugInstall +qall
