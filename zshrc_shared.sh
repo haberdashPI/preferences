aa50998e472700624077a255632c69caa6b8e582
David Little <david.frank.little@gmail.com>
2021-07-01 15:31:21 -0400

    zsh: removed experimental zsh-vi-mode

--- zshrc_shared.sh
+++ zshrc_shared.sh
@@ -3,7 +3,7 @@ KEYTIMEOUT=1
 
 export JULIA_PKG_DEVDIR="${HOME}/Documents/tools"
 
-plugins=(git fzf z brew fd docker zsh-vi-mode zsh-autosuggestions zsh-syntax-highlighting zsh-interactive-cd asdf)
+plugins=(git fzf z brew fd docker vi-mode zsh-autosuggestions zsh-syntax-highlighting zsh-interactive-cd asdf)
 source $ZSH/oh-my-zsh.sh
 
 export ZVM_VI_EDITOR=nvim
