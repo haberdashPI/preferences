a524f43c3f0851befec94547ea581372a19c8de9
David Little <david.little@beacon.bio>
2021-07-08 13:43:00 +0000

    chore: cleanup julia scripts

--- julia_remote.sh
+++ julia_remote.sh
@@ -1,16 +1,5 @@
 #! /usr/bin/zsh
-# new-session -A -s $1
 
 source ~/.zshrc
 julia_exec=`asdf env julia which julia`
-# echo $julia_exec
-# if [[ -z $VSCODE_WORKSPACE_NAME ]]; then
-#     name="vscode-workspace"
-# else
-#     name=$VSCODE_WORKSPACE_NAME
-# fi
-# echo $julia_exec
-# echo $@
-# bash
 $julia_exec "${@[1,-2]:q}" ${@[-1]}
-# tmux new-session -A -s $name $julia_exec $@
