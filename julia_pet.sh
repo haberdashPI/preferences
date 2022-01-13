ca57ba07815a958df16f15133dba75dba572745e
David F Little <david.frank.little@gmail.com>
2022-01-13 14:21:24 +0000

    handle pushover tokens

--- julia_pet.sh
+++ julia_pet.sh
@@ -2,7 +2,7 @@
 
 export ZSH="/home/ubuntu/.oh-my-zsh"
 source $HOME/Documents/preferences/zshrc_shared.sh
-source $HOME/.pushover_vars # these are secretes and must be added manually to a machine
+source $HOME/.pushovervars # these are secretes and must be added manually to a machine
 
 julia_exec=`/home/linuxbrew/.linuxbrew/bin/asdf env julia which julia`
 $julia_exec "${@[1,-2]:q}" ${@[-1]}
