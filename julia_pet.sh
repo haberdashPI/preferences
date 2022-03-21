d8845b2d531980215306de71840b1f530ddbce6e
David F Little <david.frank.little@gmail.com>
2022-03-21 15:18:06 +0000

    update pet script

--- julia_pet.sh
+++ julia_pet.sh
@@ -4,5 +4,4 @@ export ZSH="/home/ubuntu/.oh-my-zsh"
 source $HOME/Documents/preferences/zshrc_shared.sh
 source $HOME/.pushovervars # these are secretes and must be added manually to a machine
 
-julia_exec=`/home/linuxbrew/.linuxbrew/bin/asdf env julia which julia`
-$julia_exec "${@[1,-2]:q}" ${@[-1]}
+julia
