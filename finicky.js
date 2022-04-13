9051c0d6519331f7287f29bdae4472cc3e08579c
David F Little <david.frank.little@gmail.com>
2022-04-13 16:26:44 -0400

    added finicky config

---
+++ finicky.js
@@ -0,0 +1,20 @@
+module.exports = {
+    defaultBrowser: "Safari",
+    options: {
+        hideIcon: true,
+    },
+    rewrite: [
+      {
+        // Redirect all urls to use https
+        match: ({ url }) => url.protocol === "http",
+        url: { protocol: "https" }
+      }
+    ],
+    handlers: [
+      {
+        // These websites don't work as well in Safari
+        match: ["meet.google.com/*", "*.sharepoint.com/*"],
+        browser: "Google Chrome"
+      }
+    ]
+  };
